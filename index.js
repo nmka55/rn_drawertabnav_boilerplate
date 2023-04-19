b02aa1a7401f137bbd2992d668829a2dae852e66
Nyambayar Amarsaikhan <nmka.8099@gmail.com>
2023-04-20 00:51:37 +0800

    Dep updates

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /**
  * @format
  */
-
 import 'react-native-gesture-handler';
 
-import {AppRegistry} from 'react-native';
 import App from './App';
+import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
